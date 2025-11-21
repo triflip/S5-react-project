3ea0377d2118889387913f8cac41d67b5f6bf3c7
toni <canillask8@gmail.com>
2025-11-21 18:24:48 +0100

    feat: add Header organism with placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import Header  from './components/organisms/Header/Header';
 
-import './App.css'
 
 function App() {
  
 
   return (
     <>
-     
+     <Header />
     </>
   )
 }
---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,6 @@
+function Header() {
+    return <div>React</div>
+}
+
+export default Header;
+
