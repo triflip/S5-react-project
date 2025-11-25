c14c7164e0d1113115273fe689c3fbe8f33ad12c
toni <canillask8@gmail.com>
2025-11-25 22:16:37 +0100

    Refactor: create reusable CloseIcon & HamburguerIcon components

--- src/components/atoms/CloseIcon/CloseIcon.tsx
+++ src/components/atoms/CloseIcon/CloseIcon.tsx
@@ -1,18 +1,26 @@
-
 import closeIcon from "../../../assets/img/icon-close.svg";
+import styles from "./CloseIcon.module.css";
 
 interface CloseIconProps {
     size?: number;
+    onClick?: () => void;
 }
 
-const CloseIcon: React.FC<CloseIconProps> = ({ size = 24 }) => {
+const CloseIcon: React.FC<CloseIconProps> = ({ onClick, size = 24 }) => {
     return (
-        < img
-            src={closeIcon}
-            alt="Close menu"
-            width= {size}
-            height={size}
-        />
+        <button 
+            className={styles.button}
+            onClick={onClick}
+            aria-label="Close menu"
+        >
+            <img 
+                src={closeIcon}
+                alt=""
+                width={size}
+                height={size}
+                aria-hidden="true"
+            />
+        </button>
     );
 };
 
