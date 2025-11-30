10ad9aaca62bc6c1fdc8354b2e38df674638db3a
toni <canillask8@gmail.com>
2025-11-30 20:14:26 +0100

    feat: add footer elements

--- src/components/atoms/SocialIcon/SocialIcons.tsx
+++ src/components/atoms/SocialIcon/SocialIcons.tsx
@@ -1,6 +1,12 @@
-import styles from "./SocialIcon.module.css";
+import styles from "./SocialIcons.module.css";
 
-function SocialIcon({ href, src, alt }) {
+interface SocialIconProps {
+  href: string;
+  src: string;
+  alt: string;
+}
+
+function SocialIcon({ href, src, alt }: SocialIconProps) {
   return (
     <a href={href} className={styles.icon}>
       <img src={src} alt={alt} />
--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,7 +1,39 @@
+import SocialIcon from "../../atoms/SocialIcon/SocialIcons";
+import styles from "./Footer.module.css";
+
+import footerLogo from "./../../../assets/img/logo-bookmark-footer.svg";
+import facebookIcon from "./../../../assets/img/icon-facebook.svg";
+import twitterIcon from "./../../../assets/img/icon-twitter.svg";
+
 function Footer() {
-    return (
-        <div>Footer placeholder</div>
-    )
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.logo}>
+        <img src={footerLogo} alt="Bookmark logo" />
+      </div>
+
+      <nav className={styles.navFooter}>
+        <ul>
+          <a href="#features" className={styles.link}>Features</a>
+          <a href="#download" className={styles.link}>Download</a>
+          <a href="#contact" className={styles.link}>Contact</a>
+        </ul>
+      </nav>
+
+      <div className={styles.social}>
+        <SocialIcon
+          href="#"
+          src={facebookIcon}
+          alt="Follow us on Facebook"
+        />
+        <SocialIcon
+          href="#"
+          src={twitterIcon}
+          alt="Follow us on Twitter"
+        />
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
